3283022561c26f6cea4bac15f46c5973ed1a6604
Valen <valenreynoso17@gmail.com>
2023-04-30 11:35:36 -0300

    feat: pasar persona como atributo

--- src/app/componentes/main-container/main-container.component.ts
+++ src/app/componentes/main-container/main-container.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Persona } from 'src/app/model/Persona';
 
 @Component({
   selector: 'app-main-container',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class MainContainerComponent {
 
+  @Input() persona: Persona;
+  
 }
