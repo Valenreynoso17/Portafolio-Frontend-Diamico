c31f3c6e7050ad6467d639ddb6375d1696b809bd
Valen <valenreynoso17@gmail.com>
2023-04-30 11:29:34 -0300

    feat: remover codigo harcodeado, agregar pestaña CV y generalizado

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Persona } from 'src/app/model/Persona';
+import { LoginDialogComponent } from '../login-dialog/login-dialog.component';
+import { AuthorizationService } from 'src/app/services/authorization.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  @Input() persona: Persona;
+
+  linkGithub: String = "";
+  linkLinkedIn: String = "";
+  linkInstagram: String = "";
+
+  navBarTitle: String = "Argentina Programa - Portafolio";
+
+  constructor(public dialog: MatDialog, public authService: AuthorizationService) {}
+
+  ngOnInit(): void {
+    this.linkGithub = this.persona.linkGithub;
+    this.linkLinkedIn = this.persona.linkLinkedIn;
+    this.linkInstagram = this.persona.linkInstagram;
+  }
+
+  openLoginDialog() {
+    const dialogRef = this.dialog.open(LoginDialogComponent, {
+      autoFocus: false
+    });
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 
 }
