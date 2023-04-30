a97bd73a9b8175923c91db4c6e3824c7c3a7b108
Valen <valenreynoso17@gmail.com>
2023-04-30 11:21:44 -0300

    feat: generalizar knowledge-item

--- src/app/componentes/knowledge/knowledge-item/knowledge-item.component.ts
+++ src/app/componentes/knowledge/knowledge-item/knowledge-item.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Conocimiento } from 'src/app/model/Conocimiento';
 
 @Component({
   selector: 'app-knowledge-item',
   templateUrl: './knowledge-item.component.html',
   styleUrls: ['./knowledge-item.component.css']
 })
-export class KnowledgeItemComponent {
+export class KnowledgeItemComponent implements OnInit {
 
+  @Input() conocimiento: Conocimiento;
+
+  logoUrl: String = "";
+  name: String = "";
+
+  ngOnInit(): void {
+    this.logoUrl = this.conocimiento.logo;
+    this.name = this.conocimiento.nombre;
+  }
+  
 }
