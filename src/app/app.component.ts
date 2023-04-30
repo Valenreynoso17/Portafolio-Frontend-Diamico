51ec1b0ffbbde9f8b8629b97956790917e4ca3fd
Valen <valenreynoso17@gmail.com>
2023-04-30 11:36:39 -0300

    feat: conseguir y pasar persona como atributo a navbar y main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Persona } from './model/Persona';
+import { PersonService } from './services/person.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'Portafolio-Frontend-Dinamico';
+
+  persona: Persona;
+
+  constructor(private personService: PersonService) {}
+
+  ngOnInit(): void {
+    this.personService.getPersona().subscribe(p => {
+      this.persona = p;
+    })
+  }
 }
