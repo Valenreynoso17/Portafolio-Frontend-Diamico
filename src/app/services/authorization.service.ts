3ece6a7261b2d04d099ba4476e55a16f9f40ede9
Valen <valenreynoso17@gmail.com>
2023-04-30 11:40:55 -0300

    feat: creado authorization service

---
+++ src/app/services/authorization.service.ts
@@ -0,0 +1,64 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorizationService {
+
+  private baseApiUrl = "http://localhost:8080";
+
+  private _isLoggedIn = new BehaviorSubject<boolean>(false);
+  isLoggedIn = this._isLoggedIn.asObservable();
+
+   constructor(private http:HttpClient, private router: Router) { 
+    const token = localStorage.getItem("auth_token");
+    this._isLoggedIn.next(!!token);
+   }
+
+   login(email: string, password: string) {
+     return this.http.post(this.baseApiUrl + "/autenticar", {id: null, email: email, password: password}, {responseType: 'text'})
+          .pipe(
+            tap(response => {
+              if(response != "FAIL") {
+
+                this._isLoggedIn.next(true);
+
+                localStorage.setItem("auth_token", response);
+
+              } else {
+                alert("El email o usuario ingresado es incorrecto.")
+              }
+              
+            })
+          )
+              /* .subscribe((resp: any) => {
+                
+                if(resp != "FAIL") {
+                  console.log(resp);
+                  this.token = resp;
+                  localStorage.setItem("auth_token", resp);
+                  alert("usuario loggeado");
+                } else {
+                  console.log(resp);
+                  
+                }
+                 
+
+                 // this.router.navigate(["profile"]);
+                 //
+              }) */
+   }
+
+  logout() {
+    this._isLoggedIn.next(false);
+    localStorage.removeItem("auth_token");
+  }
+
+  /* isLoggedIn(): boolean {
+    return (localStorage.getItem("auth_token") !== null);
+  } */
+}
