5da4a2d8b5b42a6ae2a4e85cd7491401a5997978
Valen <valenreynoso17@gmail.com>
2023-04-30 11:24:37 -0300

    feat: generalizar about component

--- src/app/componentes/about/about.component.ts
+++ src/app/componentes/about/about.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AuthorizationService } from 'src/app/services/authorization.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+
+  @Input() acercaDe: String;
+
+  //componentTitle: String = "Acerca de";
+
+  personAbout: String = "";
+
+  constructor(public authService: AuthorizationService) { }
+
+  ngOnInit(): void {
+    this.personAbout = this.acercaDe;
+  }
+
+  onEditEvent() {
+    console.log("About edit event");
+  }
 
 }
