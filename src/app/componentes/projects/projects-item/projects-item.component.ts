4a4ffae65ae207d60cc88140790386868f07aa15
Valen <valenreynoso17@gmail.com>
2023-05-01 15:26:23 -0300

    fix: remover atributos redundantes

--- src/app/componentes/projects/projects-item/projects-item.component.ts
+++ src/app/componentes/projects/projects-item/projects-item.component.ts
@@ -6,19 +6,8 @@ import { Proyecto } from 'src/app/model/Proyecto';
   templateUrl: './projects-item.component.html',
   styleUrls: ['./projects-item.component.css']
 })
-export class ProjectsItemComponent implements OnInit {
+export class ProjectsItemComponent {
 
   @Input() proyecto: Proyecto;
 
-  link: String = "";
-  projectName: String = "";
-  projectDescription: String = '';
-
-  ngOnInit(): void {
-    this.link = this.proyecto.link;
-    this.projectName = this.proyecto.nombre;
-    this.projectDescription = this.proyecto.descripcion;
-  }
-
-  
 }
