66596d919d1eb09b8104a55b321abee330a696b1
Valentin Reynoso <valenreynoso17@gmail.com>
2023-05-01 11:50:56 -0300

    feat: permitir edit del about component

--- src/app/componentes/about/about.component.ts
+++ src/app/componentes/about/about.component.ts
@@ -1,27 +1,33 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { AuthorizationService } from 'src/app/services/authorization.service';
+import { EditAboutDialogComponent } from '../util/edit-about-dialog/edit-about-dialog.component';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent {
 
   @Input() acercaDe: String;
+  @Output() aboutEditEvent = new EventEmitter<string>();
 
-  //componentTitle: String = "Acerca de";
-
-  personAbout: String = "";
-
-  constructor(public authService: AuthorizationService) { }
-
-  ngOnInit(): void {
-    this.personAbout = this.acercaDe;
-  }
+  constructor(public dialog:MatDialog, public authService: AuthorizationService) { }
 
   onEditEvent() {
-    console.log("About edit event");
+    const editAboutDialogRef = this.dialog.open(EditAboutDialogComponent, {
+      data: { textoAcercaDe: this.acercaDe },
+      autoFocus: false
+    })
+
+    editAboutDialogRef.afterClosed().subscribe(res => {
+      if(res.cambio) {
+        console.log(res);
+        this.aboutEditEvent.emit(res.newValue);
+      }
+    });
+
   }
 
 }
