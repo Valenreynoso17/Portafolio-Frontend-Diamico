89e9e70c9a573ecc33a6d8be90c9bd25a32ffc29
Valentin Reynoso <valenreynoso17@gmail.com>
2023-04-30 21:39:14 -0300

    feat: crear dialog para edicion de "Acerca de"

---
+++ src/app/componentes/util/edit-about-dialog/edit-about-dialog.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditAboutDialogComponent } from './edit-about-dialog.component';
+
+describe('EditAboutDialogComponent', () => {
+  let component: EditAboutDialogComponent;
+  let fixture: ComponentFixture<EditAboutDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EditAboutDialogComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EditAboutDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/componentes/util/edit-about-dialog/edit-about-dialog.component.ts
@@ -0,0 +1,43 @@
+import { Component, EventEmitter, Inject, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { PersonService } from 'src/app/services/person.service';
+
+@Component({
+  selector: 'app-edit-about-dialog',
+  templateUrl: './edit-about-dialog.component.html',
+  styleUrls: ['./edit-about-dialog.component.css']
+})
+export class EditAboutDialogComponent {
+
+  editForm: FormGroup;
+
+  constructor(
+    public dialogRef:MatDialogRef<EditAboutDialogComponent>,
+    private formBuilder: FormBuilder,
+    private personaService: PersonService,
+    @Inject(MAT_DIALOG_DATA) public data: { textoAcercaDe: String }) { 
+
+      this.editForm = this.formBuilder.group({
+        personAboutControl: [data.textoAcercaDe, [Validators.required]]
+      })
+  }
+
+  edit(event: Event) {
+
+    event.preventDefault;
+
+    if (this.editForm.valid) {
+
+      this.dialogRef.close({cambio: true, newValue:this.editForm.get("personAboutControl")?.value});
+
+    } else {
+      this.editForm.markAllAsTouched();
+    }  
+  }
+
+  onCloseClick(): void {
+    this.dialogRef.close({cambio: false});
+  }
+
+}
